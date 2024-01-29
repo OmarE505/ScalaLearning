0b477a89647e50657ed516f3ddef247cf8581a74
Omar Emad <Omareg402@gmail.com>
2024-01-29 16:32:14 +0200

    Section 3: Object-Oriented Basics (exercises)

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -9,7 +9,7 @@ object OOBasics extends App {
 }
 
 // constructor
-class Person(name: String, val age: Int){
+class Person(name: String, val age: Int) {
   // body
   val x = 2
 
@@ -23,7 +23,63 @@ class Person(name: String, val age: Int){
 
   // multiple constructors
   def this(name: String) = this(name, 0)
+
   def this() = this("John Doe")
+}
+
+/*
+  Novel and a writer
+
+  Writer: first name, surname, year
+    - method fullname
+
+  Novel: name, year of release, author
+    - authorAge
+    - isWrittenBy(author)
+    - copy (new year of release) = new instance of Novel
+
+ */
+
+class Writer(firstName: String, surname: String, val year: Int) {
+  def fullname: String = firstName + " " + surname
+}
+
+class Novel(name: String, year: Int, author: Writer){
+  def authorAge = year - author.year
+  def isWrittenBy(author: Writer) = author == this.author
+  def copy(newYear: Int): Novel = new Novel(name, newYear, author)
+}
+
+/*
+  Counter class
+    - receives an int value
+    - method current count
+    - method to increment/decrement => new Counter
+    - overload inc/dec to receive an amount
+ */
+
+class Counter(val count: Int = 0) {
+  def inc = {
+    println("incrementing")
+    new Counter(count + 1)
+  }
+
+  def dec = {
+    println("decrementing")
+    new Counter(count - 1)
+  }
+
+  def inc(n: Int): Counter = {
+    if(n <= 0) this
+    else inc.inc(n - 1)
+  }
+
+  def dec(n: Int): Counter = {
+    if(n <= 0) this
+    else dec.dec(n - 1)
+  }
+  
+  def print = println(count)
 }
 
 
